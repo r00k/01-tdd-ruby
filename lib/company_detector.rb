ff9a67cfb059c308e6115b42ff7c6c85d87b7525
Ben Orenstein <ben.orenstein@gmail.com>
2018-02-13 12:37:15 -0500

    Remove tests/code

--- lib/company_detector.rb
+++ lib/company_detector.rb
@@ -1,13 +1,2 @@
 class CompanyDetector
-  def initialize(company_names)
-    @company_names = company_names
-
-    raise ArgumentError if @company_names.empty?
-  end
-
-  def run(text)
-    @company_names.select do |company_name|
-      text.downcase.include?(company_name)
-    end
-  end
 end
